6fe90a141fed28d3ec4c11ea6fda2727cd0402bf
Divyash Chhetri <divyashchhetri@gmail.com>
2022-05-06 10:00:07 +0600

    Sign In, Sign Up, Homepage, Marketplace Update

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -28,13 +28,13 @@ class CommonFooter extends HTMLElement {
 				<div class="sign_up_section">
 					<span>Already have an account?</span>
 					<h2>Start renting</h2>
-					<a href="sign_up.html">Sign Up</a>
+					<a href="sign_in.html">Sign In</a>
 				</div>
 				<hr>
 				<div class="sign_in_section">
 					<span>Want to rent a House?</span>
 					<h2>Let’s get started</h2>
-					<a href="sign_in.html">Sign In</a>
+					<a href="sign_up.html">Sign Up</a>
 				</div>
 			</div>
 
---
+++ scripts/sign_up.js
@@ -0,0 +1,5 @@
+// const phoneInputField = document.querySelector("#phone_no");
+// const phoneInput = window.intlTelInput(phoneInputField, {
+// 	utilsScript:
+// 		"https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+// });
